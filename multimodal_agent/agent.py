ca4cc512a2d29537edd1bdc51d6f3cfe4582ea54
Alvin Prayuda <alvinprayuda@gmail.com>
2025-09-01 04:26:31 +0000

    feat: implement image flipping tool with base64 encoding and artifact management

--- multimodal_agent/agent.py
+++ multimodal_agent/agent.py
@@ -1,13 +1,24 @@
 from google.adk.agents.llm_agent import Agent
 
-from multimodal_agent.callbacks import before_agent_callback
+from multimodal_agent.callbacks import before_agent_callback, before_tool_callback, after_tool_callback
 from multimodal_agent.tools import convert_image_to_grayscale, mcp_tools
 
 
 root_agent = Agent(
     name="MultimodalAgent",
-    model="gemini-2.5-flash-lite",
-    instruction="Assist user with their multimodal requests",
+    model="gemini-2.5-flash",
+    instruction="""Assist user with their multimodal requests
+If tools that have input argument with base-64 encoded data type,
+ALWAYS provide the artifact filename string value to that input argument field.
+
+E.g.:
+    "flip_image" tool has "file_data" input arguments with base64 data type.
+    Then you must provide {"file_data":"<artifact_filename>"} when you want to execute
+    this tool
+    
+DO NOT make assumption or modification on the artifact filename. 
+ONLY use the artifact filename provided to you.   
+""",
     description="""You are expert at handling multimodal requests like file or images.
 When receiving file from the user, each uploaded file will be saved as artifact and 
 you will be provided with the artifact filename information directly after the file.
@@ -19,5 +30,7 @@ E.g.
 {"artifact_filename": "uploaded-file-2-filename"}
 """,
     before_agent_callback=before_agent_callback,
+    before_tool_callback=before_tool_callback,
+    after_tool_callback=after_tool_callback,
     tools=[mcp_tools,convert_image_to_grayscale],
 )
--- multimodal_agent/callbacks.py
+++ multimodal_agent/callbacks.py
@@ -1,7 +1,16 @@
 from google.adk.agents.callback_context import CallbackContext
 from google.adk.models import LlmResponse
 from google.genai.types import Part
-from typing import Optional
+from typing import Optional, Any
+from google.adk.tools.tool_context import ToolContext
+from google.adk.tools.base_tool import BaseTool
+import base64
+import logging
+import json
+from mcp.types import CallToolResult
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def before_agent_callback(
@@ -35,3 +44,45 @@ async def before_agent_callback(
             )
 
     callback_context.user_content.parts = modified_parts
+
+async def before_tool_callback(
+    tool: BaseTool, args: dict[str, Any], tool_context: ToolContext
+):
+    # Identify which tool input should be modified
+    if tool.name == "flip_image":
+        logger.info("Modify tool args for artifact: %s", args["file_data"])
+        # Get the artifact filename from the tool input argument
+        artifact_filename = args["file_data"]
+        artifact = await tool_context.load_artifact(filename=artifact_filename)
+        file_data = artifact.inline_data.data
+
+        # Convert byte data to base64 string
+        base64_data = base64.b64encode(file_data).decode('utf-8')
+
+        # Then modify the tool input argument
+        args["file_data"] = base64_data
+
+async def after_tool_callback(
+    tool: BaseTool, args: dict[str, Any], tool_context: ToolContext, tool_response: dict | CallToolResult
+):
+
+    if tool.name == "flip_image":
+        tool_result = json.loads(tool_response.content[0].text)
+
+        # Get the expected response field which contains the flipped image base64 string
+        flipped_image_base64 = tool_result["flipped_image"]
+        artifact_filename = f"flipped_image_{tool_context.function_call_id}.png"
+        
+        # Convert base64 string to byte data
+        flipped_image_bytes = base64.b64decode(flipped_image_base64)
+        
+        # Save the flipped image as artifact
+        await tool_context.save_artifact(
+            filename=artifact_filename, artifact=Part(inline_data={"mime_type": "image/png", "data": flipped_image_bytes})
+        )
+
+        # Then modify the tool response to include the artifact filename and remove the base64 string
+        tool_result["flipped_image"] = artifact_filename
+        logger.info("Modify tool response for artifact: %s", tool_result["flipped_image"])
+        
+        return tool_result
